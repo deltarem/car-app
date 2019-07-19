9b3d7c19c45e92a55a43ff676664789bc63e0502
Dogan Ugur <doganu@quantmarketing.com>
2019-07-19 17:10:13 +0100

    updated

--- src/app/core/car-list.service.spec.ts
+++ src/app/core/car-list.service.spec.ts
@@ -1,12 +1,29 @@
 import { TestBed } from '@angular/core/testing';
 
 import { CarListService } from './car-list.service';
+import { Vehicle } from './car-list.models';
 
 describe('CarListService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
 
-  it('should be created', () => {
-    const service: CarListService = TestBed.get(CarListService);
-    expect(service).toBeTruthy();
+  let service: CarListService;
+  beforeEach(() => { service = new CarListService(); });
+
+  it('vehiclesApiUrl should return real value', () => {
+    expect(service.vehiclesApiUrl).toBe('https://itg-prd-recruit.appspot.com/api/vehicles/');
   });
+
+  it('vehicleApiUrl should return real value', () => {
+    expect(service.vehicleApiUrl).toBe('https://itg-prd-recruit.appspot.com/api/vehicle/');
+  });
+
+
+  it('getVehicleList() item list should return value from observable',
+    (done: DoneFn) => {
+    service.getVehicleList().subscribe((value: Vehicle[])  => {
+      expect(value.length).toBe(5);
+      done();
+    });
+  });
+
+
 });
