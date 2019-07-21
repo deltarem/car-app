6cdc265da3bb9a36217c6cd963db150ac37eac00
Dogan UGUR <dogan.ugur@ihsmarkit.com>
2019-07-21 20:25:51 +0100

    updated to deploy

--- src/app/core/car-list.service.spec.ts
+++ src/app/core/car-list.service.spec.ts
@@ -1,29 +1,30 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule,
+  HttpTestingController } from '@angular/common/http/testing';
 import { CarListService } from './car-list.service';
 import { Vehicle } from './car-list.models';
 
 describe('CarListService', () => {
 
+  let httpTestingController: HttpTestingController;
   let service: CarListService;
-  beforeEach(() => { service = new CarListService(); });
-
-  it('vehiclesApiUrl should return real value', () => {
-    expect(service.vehiclesApiUrl).toBe('https://itg-prd-recruit.appspot.com/api/vehicles/');
-  });
-
-  it('vehicleApiUrl should return real value', () => {
-    expect(service.vehicleApiUrl).toBe('https://itg-prd-recruit.appspot.com/api/vehicle/');
-  });
-
-
-  it('getVehicleList() item list should return value from observable',
-    (done: DoneFn) => {
-    service.getVehicleList().subscribe((value: Vehicle[])  => {
-      expect(value.length).toBe(5);
-      done();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [CarListService],
+      imports: [HttpClientTestingModule]
     });
+
+    // We inject our service (which imports the HttpClient) and the Test Controller
+    httpTestingController = TestBed.get(HttpTestingController);
+    service = TestBed.get(CarListService);
+
+    expect(service.vehiclesApiUrl).toBe('https://itg-prd-recruit.appspot.com/api/vehicles/');
+
+    expect(service.vehicleApiUrl).toBe('https://itg-prd-recruit.appspot.com/api/vehicle/');
+
+    service.getVehicleList().subscribe((value: Vehicle[])  => {
+        expect(value.length).toBe(5);
+      });
+
   });
-
-
 });
